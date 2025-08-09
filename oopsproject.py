fbd3db7ed275c90b6c2f54f866e2fc2b7a31f3df
OG810 <omkargochademanit@gmail.com>
2025-08-09 23:02:43 +0530

    few functianlities of sign in, sign up and write post  added

--- oopsproject.py
+++ oopsproject.py
@@ -16,15 +16,48 @@ class chatbook:
         print("You entered:", user_input)  # This will print the entered value
 
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
-            pass
+            self.write_post()
         elif user_input == "4":
             pass
         else:
             exit()
+    def signup(self):
+        email=input("Enter your email")
+        passw=input("eneter your password")
+        self.username=email
+        self.password=passw
+        print("u have signed up successfully !!")
+        print("\n")
+        self.menu()
+    def signin(self):
+        if self.username=='' and  self.password=='':
+            print("please sign up first by pressing 1")
+        else:
+            uname=input("Enter ur username/emai here ")
+            passw=input("enetr ur passwrod here ")
+            if self.username==uname and self.password==passw:
+                print("u have signed up successfully !")
+                self.loggedin=True
+            else:
+                print("give correct credentials")
+        print("\n")
+        self.menu()     
+    def write_post(self):
+        if self.loggedin:
+            post=input("Rite ur post here ")
+            print("post written successfully",post)
+        else:
+            print("please sign in forst by using 2")
+        print("\n")
+        self.menu()
+        
+
+
+
 
 obj = chatbook()
 obj.menu()
